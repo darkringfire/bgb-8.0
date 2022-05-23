53243704c3bc3c0ced537d22929028ca54c73f61
A.Kosorotikov <darkringfire@gmail.com>
2022-05-23 08:49:31 +0800

    Refactor - inetChanged: logger

--- dyn/ru/badcom/scripts/behavior/inetChanged.java
+++ dyn/ru/badcom/scripts/behavior/inetChanged.java
@@ -1,43 +1,43 @@
 package ru.badcom.scripts.behavior;
 
+import bitel.billing.server.contract.bean.CommentPatternManager;
+import bitel.billing.server.contract.bean.Contract;
+import bitel.billing.server.contract.bean.ContractManager;
+import bitel.billing.server.contract.bean.ContractParameterManager;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import ru.bitel.bgbilling.kernel.script.server.dev.EventScript;
 import ru.bitel.bgbilling.kernel.script.server.dev.EventScriptBase;
+import ru.bitel.bgbilling.modules.inet.api.common.bean.InetServ;
 import ru.bitel.bgbilling.modules.inet.api.server.event.InetServChangingEvent;
-
 import ru.bitel.bgbilling.server.util.Setup;
 import ru.bitel.common.sql.ConnectionSet;
 
-import ru.bitel.bgbilling.modules.inet.api.common.bean.InetServ;
-import java.sql.*;
-import bitel.billing.server.contract.bean.ContractManager;
-import bitel.billing.server.contract.bean.ContractParameterManager;
-import bitel.billing.server.contract.bean.Contract;
-import bitel.billing.server.contract.bean.CommentPatternManager;
+import java.sql.Connection;
 
 public class inetChanged extends EventScriptBase<InetServChangingEvent> implements EventScript<InetServChangingEvent>
 {
+	protected static final Logger logger = LogManager.getLogger(InetServChangingEvent.class);
+	private static final int loginParameterId = 12;
+
 	@Override
 	public void onEvent( InetServChangingEvent event, Setup setup, ConnectionSet connectionSet )
       throws Exception
 	{
-		System.out.println("Inet service changed");
+		logger.info("Inet service changed");
 		Connection connection = connectionSet.getConnection();
 		ContractManager contractManager = new ContractManager(connection);
 		ContractParameterManager contractParameterManager = new ContractParameterManager(connection);
 		CommentPatternManager commentPatternManager = new CommentPatternManager(connection);
-		int contracrId = event.getContractId();
-		Contract contract = contractManager.getContractById(contracrId);	
+		int contractId = event.getContractId();
+		Contract contract = contractManager.getContractById(contractId);
 		InetServ inetServ = event.getNewInetServ();
 		
-		//System.out.println(inetServ.getPassword());
-		//System.out.println(contract.getPswd());
 		contract.setPswd(inetServ.getPassword());
-		//System.out.println(inetServ.getLogin());
-		
+
 		contractManager.updateContract(contract);
 		
-		contractParameterManager.updateStringParam(contracrId, 12, inetServ.getLogin(), 0);
-		commentPatternManager.updateContractComment(contracrId);
-		//System.out.println(contractParameterManager.getStringParam(contracrId, 12));
-    }  
+		contractParameterManager.updateStringParam(contractId, loginParameterId, inetServ.getLogin(), 0);
+		commentPatternManager.updateContractComment(contractId);
+    }
 }
