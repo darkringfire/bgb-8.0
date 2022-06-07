dfd2e68e4ad6180650ce68974ea47914f2f3248d
A.Kosorotikov <darkringfire@gmail.com>
2022-06-07 11:25:17 +0800

    Refactor deprecated classes
    InetServChanging_SetContractLoginPassword_Event

--- dyn/ru/badcom/bgbilling/events/InetServChanging_SetContractLoginPassword_Event.java
+++ dyn/ru/badcom/bgbilling/events/InetServChanging_SetContractLoginPassword_Event.java
@@ -3,15 +3,16 @@ package ru.badcom.bgbilling.events;
 import bitel.billing.server.contract.bean.CommentPatternManager;
 import bitel.billing.server.contract.bean.Contract;
 import bitel.billing.server.contract.bean.ContractManager;
-import bitel.billing.server.contract.bean.ContractParameterManager;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import ru.bitel.bgbilling.common.BGException;
+import ru.bitel.bgbilling.kernel.contract.api.server.bean.ContractDao;
 import ru.bitel.bgbilling.kernel.script.server.dev.EventScriptBase;
 import ru.bitel.bgbilling.modules.inet.api.common.bean.InetServ;
 import ru.bitel.bgbilling.modules.inet.api.server.event.InetServChangingEvent;
 import ru.bitel.bgbilling.server.util.Setup;
 import ru.bitel.common.sql.ConnectionSet;
+import ru.bitel.oss.kernel.entity.common.bean.EntityAttrText;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -21,23 +22,30 @@ import java.sql.SQLException;
  */
 public class InetServChanging_SetContractLoginPassword_Event<E extends InetServChangingEvent> extends EventScriptBase<E>{
     protected static final Logger logger = LogManager.getLogger(InetServChanging_SetContractLoginPassword_Event.class);
-    private static final int PARAM_LOGIN_ID = 12;
+    private static final int PARAM_ID_LOGIN = 12;
 
     @Override
     public void onEvent(E event, Setup setup, ConnectionSet connectionSet) throws BGException, SQLException {
-        Connection connection = connectionSet.getConnection();
-        ContractManager contractManager = new ContractManager(connection);
-        // TODO fix deprecated
-        ContractParameterManager contractParameterManager = new ContractParameterManager(connection);
-        CommentPatternManager commentPatternManager = new CommentPatternManager(connection);
-        int contractId = event.getContractId();
-        Contract contract = contractManager.getContractById(contractId);
-        InetServ inetServ = event.getNewInetServ();
+        int cid = event.getContractId();
+        int uid = event.getUserId();
 
+        Connection con = connectionSet.getConnection();
+        InetServ inetServ = event.getNewInetServ();
+        ContractManager contractManager = new ContractManager(con);
+        CommentPatternManager commentPatternManager = new CommentPatternManager(con);
+        ContractDao contractDao = new ContractDao(con, uid);
+
+        // Set login parameter by inet login
+        EntityAttrText loginParameter = (EntityAttrText) contractDao.getContractParameter(cid, PARAM_ID_LOGIN);
+        loginParameter.setValue(inetServ.getLogin());
+        contractDao.updateContractParameter(cid, loginParameter);
+
+        // Set contract password
+        Contract contract = contractManager.getContractById(cid);
         contract.setPswd(inetServ.getPassword());
         contractManager.updateContract(contract);
 
-        contractParameterManager.updateStringParam(contractId, PARAM_LOGIN_ID, inetServ.getLogin(), 0);
-        commentPatternManager.updateContractComment(contractId);
+        // Update contract comment
+        commentPatternManager.updateContractComment(cid);
     }
 }
